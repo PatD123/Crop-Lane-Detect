e698fc83c2d56a4794ebec9c2f73b7a39738d385
Patrick Dai <patrickdai917@gmail.com>
2024-01-08 17:11:44 -0800

    Added some images from online. Canny-ed some images.

--- warp2.py
+++ warp2.py
@@ -11,11 +11,28 @@ import math
 ORIGINAL_SIZE = 640, 480
 UNWARPED_SIZE = 500, 600
 
-imgs = ["test_images/bikelane.jpg"]
+imgs = ["test_images2/straight_lines1.jpg"]
 img = mpimg.imread(imgs[0])
 
-# plt.imshow(img)
-# plt.show()
+# Getting a new ROI for image
+print(img.shape)
+
+# Canny + Hough Lines
+img_HLS = cv2.cvtColor(img, cv2.COLOR_BGR2HLS)
+_h_channel = img_HLS[:, :, 0]
+_l_channel  = img_HLS[:, :, 1]
+_s_channel = img_HLS[:, :, 2]
+
+low_thresh = 100
+high_thresh = 200
+# Better to do Canny on lightness channel
+edges = cv2.Canny(_l_channel, low_thresh, high_thresh)
+
+plt.imshow(edges)
+plt.show()
+
+
+
 
 
 
